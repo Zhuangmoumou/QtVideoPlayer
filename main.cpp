fe93345691af23abadffd13c53999e3569955de2
Lyrecoul <h5re0jatt@mozmail.com>
2025-06-07 23:12:35 +0800

    增加命令行处理

--- main.cpp
+++ main.cpp
@@ -1,32 +1,42 @@
 #include <QApplication>
-#include "FileBrowser.h"
+#include <QDebug>
 #include "VideoPlayer.h"
+#include "qapplication.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-    QApplication::setApplicationName("RK3326 Player");
+    QApplication::setApplicationName("NewPlayer");
 
     QStringList args = app.arguments();
-    if (args.size() > 1) {
-        // 带参数启动，直接播放
-        QString path = args.at(1);
+    // 支持短参数补全
+    bool showHelp = false;
+    QString path;
+    for (int i = 1; i < args.size(); ++i) {
+        QString arg = args.at(i);
+        if (arg == "--help" || arg == "-h") {
+            showHelp = true;
+        } else if (!arg.startsWith("-") && path.isEmpty()) {
+            path = arg;
+        }
+    }
+
+    if (showHelp) {
+        qDebug() << "用法: NewPlayer <视频文件路径>";
+        qDebug() << "参数:";
+        qDebug() << "  --help, -h          显示帮助信息";
+        return 0;
+    }
+
+    if (!path.isEmpty()) {
+        // 带参数启动，直接全屏播放
         VideoPlayer *player = new VideoPlayer;
         player->setWindowState(Qt::WindowFullScreen);
         player->play(path);
         return app.exec();
     } else {
-        // 无参数，打开文件管理
-        FileBrowser *fb = new FileBrowser("/userdisk/Music");
-        fb->setWindowState(Qt::WindowFullScreen);
-        QObject::connect(fb, &FileBrowser::fileSelected, [&](const QString &file){
-            VideoPlayer *player = new VideoPlayer;
-            player->setWindowState(Qt::WindowFullScreen);
-            player->play(file);
-            fb->close();
-        });
-        fb->show();
-        return app.exec();
+        qDebug() << "未指定视频文件路径。使用 --help 查看用法。";
+        return 0;
     }
 }
